79f56fee28f2210c2ad9fa79499355623051b842
Brian Travis <bztravis88@gmail.com>
2023-10-28 13:55:46 -0700

    Get Milvus node sdk working

--- next.config.js
+++ next.config.js
@@ -1,9 +1,31 @@
-/** @type {import('next').NextConfig} */
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+/** @type {import('next').NextConfig} */
 
 nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Copy the proto files to the server build directory
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.join(
+                __dirname,
+                'node_modules/@zilliz/milvus2-sdk-node/dist'
+              ),
+              to: path.join(__dirname, '.next'),
+            },
+          ],
+        })
+      )
+    }
+    // Important: return the modified config
+    return config
   },
 }
 
