868da61c7a9fed6d29b315fbfdde24162f7527c4
Léo Liu <leoliuhy@gmail.com>
2023-10-28 14:21:24 -0700

    switched to dotenv

--- app/api/v1/query/route.js
+++ app/api/v1/query/route.js
@@ -2,14 +2,14 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { MilvusClient } from '@zilliz/milvus2-sdk-node'
 
+require('dotenv').config()
+
 export async function GET(req) {
-  const address =
-    'https://in03-575b131d648efbe.api.gcp-us-west1.zillizcloud.com'
+  const address = process.env.MILVUS_URL
   // - For a serverless cluster, use an API key as the token.
   // - For a dedicated cluster, use the cluster credentials as the token
   // in the format of 'user:password'.
-  const token =
-    'ade46ba572172c1a5655d38a460e85df620e2367068dbec49cb913ff491249881e5182d8f07dbacd0af53751289a389eef8715b8'
+  const token = process.env.MILVUS_KEY
 
   const client = new MilvusClient({ address, token })
 
